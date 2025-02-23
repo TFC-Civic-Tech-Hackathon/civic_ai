4d2317a27cd0a455a426ee70208e8ea49bb0c6cf
Aaryan Praveen <55227143+boioboi16@users.noreply.github.com>
2025-02-23 06:57:19 -0500

    data visualization added

--- civic-sentinel/src/services/VisualizeService.ts
+++ civic-sentinel/src/services/VisualizeService.ts
@@ -9,3 +9,21 @@ export const policiesPerMonth = async () => {
   });
   return response.data;
 };
+export const agencyDistribution = async () => {
+  const response = await axios.get("/visualize/agency-distribution", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
+  return response.data;
+};
+export const subAgencyDistribution = async () => {
+  const response = await axios.get("/visualize/sub-agency-distribution", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
+  return response.data;
+};
