7bfec92b8fab3ee02e1fe4f14e3949c734d4d039
ChaudharyAnshul <chaudhary.ans@northeastern.edu>
2025-02-23 07:55:14 -0500

    notifications

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import authRouter, healthRouter, userDocRouter, chatRouter, visualizeRouter
+from routes import authRouter, healthRouter, userDocRouter, chatRouter, visualizeRouter, pdfRouter
 import configparser
 from fastapi import APIRouter
 
